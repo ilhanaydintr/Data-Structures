c7b556d828375fd03f6950021aaebb635713c214
ilhan AYDIN <31769437+ilhanaydintr@users.noreply.github.com>
2018-10-09 21:46:45 +0300

    Ödev-1 (Polinom ödevi çözümü)
    Sadece polinom ödevi eklenmiştir. Diğer ödev derste yapılan örneğe benzer olduğundan eklenmemiştir.

---
+++ odev1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package odev1;
+
+class polinom{
+    int ksayi,derece;
+    polinom ileri;
+    public polinom(int ksayi, int derece) {
+        this.ksayi = ksayi;
+        this.derece = derece;
+        ileri = null;
+    }
+
+}
+public class odev1 {
+    polinom bas;
+    public odev1(){
+        bas=null;
+    }
+   void siraliekle(polinom yeni)     { 
+         if (bas == null || bas.derece <= yeni.derece) 
+         { 
+            yeni.ileri = bas; 
+            bas = yeni; 
+         } 
+         else {
+            polinom tmp = bas;   
+            while (tmp.ileri != null && 
+                   tmp.ileri.derece > yeni.derece) 
+                  tmp = tmp.ileri; 
+  
+            yeni.ileri = tmp.ileri; 
+            tmp.ileri = yeni; 
+         } 
+     }
+   String polinomgoster(){
+       polinom p=bas;
+       String S="";
+       while(p!=null){
+           S=S+p.ksayi+"x^"+p.derece+"+";
+           p=p.ileri;
+       }
+       return S;
+   }
+}
